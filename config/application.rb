525512a7b236579c245f30352a73dfc473d5b867
Ash Darji <ashishdarji88@gmail.com>
2014-07-11 08:10:45 -0400

    heroku crap

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Breakout
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w( *.js *.css *.eot *.woff *.ttf *.svg )
+
 
   end
 end
